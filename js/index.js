d1280ffb780880d093bb7156469ba40e5fdf77fb
boylw <lww842641996@qq.com>
2018-05-19 11:43:55 +0800

    覆盖

--- js/index.js
+++ js/index.js
@@ -15,12 +15,30 @@ $(function () {
         },
         /*离开某一个页面的时候触发*/
         onLeave:function (index,nextIndex,direction) {
+            var currentSection = $('.section').eq(index-1);
             if(index == 2 && nextIndex == 3){
                 /*当前是从第二页到第三页*/
-                $('.section').eq(index-1).addClass('leaved');
+                currentSection.addClass('leaved');
             }else if(index == 3 && nextIndex == 4){
                 /*当前是从第三页到第四页*/
-                $('.section').eq(index-1).addClass('leaved');
+                currentSection.addClass('leaved');
+            }else if(index == 5 && nextIndex == 6){
+                /*当前是从第五页到第六页*/
+                /*currentSection.removeClass('now').addClass('leaved');*/
+                currentSection.addClass('leaved');
+                $('.screen06 .box').addClass('show');
+            }else if(index == 6 && nextIndex == 7){
+                /*当前是从第6页到第7页*/
+                $('.screen07 .star').addClass('show');
+                $('.screen07 .text').addClass('show');
+                $('.screen07 .star img').each(function (i, item) {
+                    /*$(item) == $(this);*/
+                    /*img display:none*/
+                    /*$(this).delay(i*0.5*1000).fadeIn();*/
+                    /*img opacity*/
+                    $(this).css('transition-delay',i*0.5+'s');
+                });
+
             }
         },
         /*最好在组件初始完毕或者插件内容渲染完毕*/
@@ -42,6 +60,30 @@ $(function () {
                 /* :last :first :visible :hidden :checked :selected jquery扩展选择器*/
                 $('.screen04 .address').show().find('img:last').fadeIn(1000);
                 $('.screen04 .text').addClass('show');
+            });
+
+
+            /*第八屏功能*/
+            /*1.手要跟着鼠标移动*/
+            $('.screen08').on('mousemove',function (e) {
+                /*鼠标的坐标设置给手*/
+                $(this).find('.hand').css({
+                    left:e.clientX -190,
+                    top:e.clientY - 20
+                });
+            }).find('.again').on('click',function () {
+                /*2.点击再来一次重置动画跳回第一页*/
+                /*动画怎么怎么进行的？*/
+                /*2.1 加now  类*/
+                /*2.2 加leaved  类*/
+                /*2.3 加show 类*/
+                $('.now,.leaved,.show').removeClass('now').removeClass('leaved').removeClass('show');
+                /*2.4 加css属性  后果：加一个style属性*/
+                /*2.5 用jquery方法  show() fadeIn() 后果：加一个style属性*/
+                $('.content [style]').removeAttr('style');
+
+                /*跳回第一页*/
+                $.fn.fullpage.moveTo(1);
             });
         },
         /*页面切换的时间 默认是700*/
